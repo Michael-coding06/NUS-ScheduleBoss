cfc4648414659c3439cc898315ebd2fffb267478
Michael-coding06 <80338451+Michael-coding06@users.noreply.github.com>
2025-06-28 18:33:35 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,19 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Timetable from './components//TimeTable/Timetable'
 import { useEffect, useState } from 'react';
 import Login from './components/LoginSignup/Login';
 import Signup from './components/LoginSignup/Signup';
+
 function App() {
   const [token, setToken] = useState(() => {
     const stored = sessionStorage.getItem('token');
     return stored ? JSON.parse(stored) : null;
   });
 
-  
   if(token){
     sessionStorage.setItem('token', JSON.stringify(token))
   }
-
 
   useEffect(() => {
     if(sessionStorage.getItem('token')){
